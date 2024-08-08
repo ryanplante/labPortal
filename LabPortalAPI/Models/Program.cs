7519132972e4fdad9118d58218c22277f32a2259
Ryan Plante <rmplante@email.neit.edu>
2024-08-07 20:43:32 -0400

    add launch argument to specify port

--- LabPortalAPI/Models/Program.cs
+++ LabPortalAPI/Models/Program.cs
@@ -5,14 +5,12 @@ using System.Text.Json.Serialization;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
 builder.Services.AddControllers()
     .AddJsonOptions(options =>
     {
         options.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.Preserve;
         options.JsonSerializerOptions.DefaultIgnoreCondition = JsonIgnoreCondition.WhenWritingNull;
     });
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddDbContext<TESTContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
@@ -32,4 +30,8 @@ app.UseAuthorization();
 
 app.MapControllers();
 
-app.Run();
+// Get the port number from command-line arguments or use a default port
+var port = args.Length > 0 && int.TryParse(args[0], out var parsedPort) && parsedPort >= 49152 && parsedPort <= 65535 ? parsedPort : 5000;
+
+// Ensure the app is listening on the specified port
+app.Run($"http://0.0.0.0:{port}");
