587571db9e8fdbe0ffe7a2687bf9030d1e64abc8
Ryan Plante <rmplante@email.neit.edu>
2024-08-19 22:32:34 -0400

    update icons and update permissions on navbar

--- lab-portal/components/Desktop/Sidebar.tsx
+++ lab-portal/components/Desktop/Sidebar.tsx
@@ -1,11 +1,28 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { View, StyleSheet, Image, Text, TouchableOpacity } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
+import { getUserByToken } from '../../services/loginService';
 
 const Sidebar = ({ onProfilePress }: { onProfilePress: () => void }) => {
   const navigation = useNavigation();
+  const [privLvl, setPrivLvl] = useState<number>(0);
+
+  useEffect(() => {
+    const fetchUserData = async () => {
+      try {
+        const user = await getUserByToken();
+        setPrivLvl(user.privLvl);
+      } catch (error) {
+        console.error('Failed to load user data:', error);
+      }
+    };
+
+    fetchUserData();
+  }, []);
+
   // Stupid hack to get the text centered >:(
   const labMonitors = "   Add/Edit\nLab Monitors" 
+
   return (
     <View style={styles.sidebar}>
       <TouchableOpacity style={styles.menuItem} onPress={() => navigation.navigate('Main')}>
@@ -15,38 +32,52 @@ const Sidebar = ({ onProfilePress }: { onProfilePress: () => void }) => {
         <Image source={require('../../assets/user-icon.png')} style={styles.icon} />
         <Text style={styles.menuText}>Profile</Text>
       </TouchableOpacity>
-      <TouchableOpacity style={styles.menuItem} onPress={() => navigation.navigate('Labs')}>
-        <Image source={require('../../assets/labs-icon.png')} style={styles.icon} />
-        <Text style={styles.menuText}>{labMonitors}</Text>
-      </TouchableOpacity>
-      <TouchableOpacity style={styles.menuItem} onPress={() => navigation.navigate('LabSchedules')}>
-        <Image source={require('../../assets/schedule-icon.png')} style={styles.icon} />
-        <Text style={styles.menuText}>Lab Schedules</Text>
-      </TouchableOpacity>
-      <TouchableOpacity style={styles.menuItem} onPress={() => navigation.navigate('Chat')}>
-        <Image source={require('../../assets/chat-icon.png')} style={styles.icon} />
-        <Text style={styles.menuText}>Chat</Text>
-      </TouchableOpacity>
-      <TouchableOpacity style={styles.menuItem} onPress={() => navigation.navigate('ScanItem')}>
-        <Image source={require('../../assets/scan-icon.png')} style={styles.icon} />
-        <Text style={styles.menuText}>Scan Item</Text>
-      </TouchableOpacity>
-      <TouchableOpacity style={styles.menuItem} onPress={() => navigation.navigate('LogHistory')}>
-        <Image source={require('../../assets/history-icon.png')} style={styles.icon} />
-        <Text style={styles.menuText}>Log History</Text>
-      </TouchableOpacity>
-      <TouchableOpacity style={styles.menuItem} onPress={() => navigation.navigate('Reports')}>
-        <Image source={require('../../assets/reports-icon.png')} style={styles.icon} />
-        <Text style={styles.menuText}>Reports</Text>
-      </TouchableOpacity>
-      <TouchableOpacity style={styles.menuItem} onPress={() => navigation.navigate('Schedule')}>
-        <Image source={require('../../assets/schedule-icon.png')} style={styles.icon} />
-        <Text style={styles.menuText}>Schedule</Text>
-      </TouchableOpacity>
-      <TouchableOpacity style={styles.menuItem} onPress={() => navigation.navigate('Admin')}>
-        <Image source={require('../../assets/admin-icon.png')} style={styles.icon} />
-        <Text style={styles.menuText}>Admin</Text>
-      </TouchableOpacity>
+      {(privLvl > 4) && (
+        <TouchableOpacity style={styles.menuItem} onPress={() => navigation.navigate('Labs')}>
+          <Image source={require('../../assets/labs-icon.png')} style={styles.icon} />
+          <Text style={styles.menuText}>{labMonitors}</Text>
+        </TouchableOpacity>
+      )}
+      {( privLvl >= 1 && privLvl <= 3) && (
+        <TouchableOpacity style={styles.menuItem} onPress={() => navigation.navigate('LabSchedules')}>
+          <Image source={require('../../assets/schedule-icon.png')} style={styles.icon} />
+          <Text style={styles.menuText}>Schedule</Text>
+        </TouchableOpacity>
+      )}
+      {(privLvl <= 3 ) && (
+        <TouchableOpacity style={styles.menuItem} onPress={() => navigation.navigate('Chat')}>
+          <Image source={require('../../assets/chat-icon.png')} style={styles.icon} />
+          <Text style={styles.menuText}>Chat</Text>
+        </TouchableOpacity>
+      )}
+      {(privLvl >= 1 && privLvl <= 3) && (
+        <TouchableOpacity style={styles.menuItem} onPress={() => navigation.navigate('ScanItem')}>
+          <Image source={require('../../assets/scan-icon.png')} style={styles.icon} />
+          <Text style={styles.menuText}>Scanner</Text>
+        </TouchableOpacity>
+      )}
+      {(privLvl >= 1) && (
+        <TouchableOpacity style={styles.menuItem} onPress={() => navigation.navigate('LogHistory')}>
+          <Image source={require('../../assets/logs-icon.png')} style={styles.icon} />
+          <Text style={styles.menuText}>View Logs</Text>
+        </TouchableOpacity>
+      )}
+      {(privLvl >= 4) && (
+        <TouchableOpacity style={styles.menuItem} onPress={() => navigation.navigate('Reports')}>
+          <Image source={require('../../assets/reports-icon.png')} style={styles.icon} />
+          <Text style={styles.menuText}>Reports</Text>
+        </TouchableOpacity>
+      )}
+      {(privLvl === 5) && (
+        <TouchableOpacity style={styles.menuItem} onPress={() => navigation.navigate('Admin')}>
+          <Image source={require('../../assets/admin-icon.png')} style={styles.icon} />
+          <Text style={styles.menuText}>Admin</Text>
+        </TouchableOpacity>
+      )}
+        <TouchableOpacity style={styles.menuItem} onPress={() => navigation.navigate('Help')}>
+          <Image source={require('../../assets/help-icon.png')} style={styles.icon} />
+          <Text style={styles.menuText}>Help</Text>
+        </TouchableOpacity>
     </View>
   );
 };
